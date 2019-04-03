f172a8ccf01d17b6d4054ed08d40d4706dac459b
theangrybagel <34526427+theangrybagel@users.noreply.github.com>
2019-04-03 13:23:43 -0700

    did stuff

--- Directory.js
+++ Directory.js
@@ -1,10 +1,19 @@
+function file(rawText) {
+  this.raw = rawText;
+}
+
 var disk = {
   users: {
     name: "Users",
     guest: {
       name: "Guest",
       documents: {
-        name: "Documents"
+        name: "Documents",
+        readme: {
+          name: "Readme",
+          type: "txt",
+          data: new file("This is a file of some sort.")
+        }
       }
     }
   }
--- code.js
+++ code.js
@@ -3,6 +3,34 @@ var displayPath = "Virtual_Disk_0." + currentPath;
 function UpdateDisplayPath() {
   displayPath = "Virtual_Disk_0." + currentPath;
 }
+function AddFile(file) {
+  if (currentDir() != "cannotNavigate") {
+    var returnTxt = "";
+    var endAmt = 0;
+    var dir = disk;
+    var p = currentPath.split(".");
+    for (var i = 0; i < p.length; i++) {
+      returnTxt += "[" + p[i].toLowerCase();
+      endAmt = i + 1;
+    }
+    for (var i = 0; i < endAmt; i++) {
+      returnTxt += "]";
+    }
+    eval(
+      "disk" +
+        returnTxt +
+        "." +
+        file.name +
+        " = {name: '" +
+        file.name +
+        "', type: " +
+        file.type +
+        " }"
+    );
+    console.log(returnTxt);
+    console.log(disk);
+  }
+}
 function currentDir() {
   var p = currentPath.split(".");
   var cd = disk;
@@ -12,15 +40,12 @@ function currentDir() {
   for (var i = 0; i < p.length; i++) {
     console.log(p[i]);
     console.log(cd[p[i].toLowerCase()]);
-    if(cd.hasOwnProperty([p[i].toLowerCase()]))
-    cd = cd[p[i].toLowerCase()];
-    else
-    {
+    if (cd.hasOwnProperty([p[i].toLowerCase()])) cd = cd[p[i].toLowerCase()];
+    else {
       canNav = false;
     }
   }
-  if(!canNav)
-  return "cannotNavigate"
+  if (!canNav) return "cannotNavigate";
   console.log(cd);
   return cd;
 }
@@ -31,11 +56,10 @@ var Commands = {
       if (parameters[1] == undefined) return "Usage: cd [directory]";
       currentPath += "." + parameters[1];
       var canNavigate = true;
-      if(currentDir() === "cannotNavigate")
-        {
-          currentPath = prevPath;
-          return "Could not find the path specified";
-        }
+      if (currentDir() === "cannotNavigate") {
+        currentPath = prevPath;
+        return "Could not find the path specified";
+      }
       AddToLog(currentDir()["name"]);
       UpdateDisplayPath();
       return "Changed directory to " + displayPath;
@@ -49,31 +73,56 @@ var Commands = {
     }
   },
   view: {
-    call: function(s)
-    {
+    call: function(s) {
       var a = Object.getOwnPropertyNames(currentDir());
       var rslt = "All files in the current directory: ";
-      for(var i = 0; i < a.length; i++)
-      {
-        if(a[i] != "name")
-        {
+      for (var i = 0; i < a.length; i++) {
+        if (a[i] != "name") {
           rslt += "<br>" + a[i];
+          if (isObject(currentDir()[a[i]])) {
+            if (currentDir()[a[i]].hasOwnProperty("type"))
+              rslt += "." + currentDir()[a[i]]["type"];
+          }
         }
       }
       return rslt;
     }
+  },
+  create: {
+    call: function(
+      p // > create [type (txt, folder, etc)] [name]
+    ) {
+      if (p[1] === undefined) return "Usage: create [type] [name]";
+      if (p[2] == undefined) return "Invalid syntax";
+      if (p[2].includes(".") || p[2].includes(",") || p[2].includes(":"))
+        return "The name cannot contain any of the following characters: '.', ',', ':'.";
+      if (p[1].includes(".") || p[1].includes(",") || p[1].includes(":"))
+        return "The type cannot contain any of the following characters: '.', ',', ':'.";
+      var rslt = "";
+      //if (p[2] === "folder") {
+      //disk[currentDir()[p[2]]] = { name: p[2] };
+      var f = { name: p[2], type: p[1] };
+      AddFile(f);
+      //}
+    }
   }
 };
 Start();
-function Start()
-{
-Call(displayPath + ")> rd");
+function Start() {
+  Call(displayPath + ")> rd");
 }
-
+function isObject(obj) {
+  return Object.prototype.toString.call(obj) === "[object Object]";
+}
 function KeyPress(kc) {
-  if (kc.key == "Enter") {
+  if (kc.key === "Enter") {
     var value = document.getElementById("input").value;
     Call(value);
+  }
+}
+function KeyDown() {
+  if (!document.getElementById("input").value.startsWith(displayPath + ")> ")) {
+    document.getElementById("input").value = displayPath + ")> ";
   }
 }
 function AddToLog(str) {
